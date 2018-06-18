c9f85092af3386a91ac677d966e8ef8dacbdd699
betula <betula@yandex-team.ru>
2018-06-19 01:48:36 +0300

    Add rust server example

--- server-rust/src/main.rs
+++ server-rust/src/main.rs
@@ -1,18 +1,84 @@
+//! Actix web diesel example
+//!
+//! Diesel does not support tokio, so we have to run it in separate threads.
+//! Actix supports sync actors by default, so we going to create sync actor
+//! that use diesel. Technically sync actors are worker style actors, multiple
+//! of them can run in parallel and process messages from same queue.
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate diesel;
+extern crate actix;
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest, Responder};
+extern crate env_logger;
+extern crate futures;
+extern crate r2d2;
+extern crate uuid;
 
-fn tokenGet(req: HttpRequest) -> impl Responder {
+use actix::prelude::*;
+use actix_web::{
+    http, middleware, server, App, AsyncResponder, FutureResponse, HttpResponse, Path,
+    State,
+};
 
-    format!("Hello {}!", to)
+use diesel::prelude::*;
+use diesel::r2d2::ConnectionManager;
+use futures::Future;
+
+mod db;
+mod models;
+mod schema;
+
+use db::{CreateUser, DbExecutor};
+
+/// State with DbExecutor address
+struct AppState {
+    db: Addr<Syn, DbExecutor>,
+}
+
+/// Async request handler
+fn index(
+    (name, state): (Path<String>, State<AppState>),
+) -> FutureResponse<HttpResponse> {
+    // send async `CreateUser` message to a `DbExecutor`
+    state
+        .db
+        .send(CreateUser {
+            name: name.into_inner(),
+        })
+        .from_err()
+        .and_then(|res| match res {
+            Ok(user) => Ok(HttpResponse::Ok().json(user)),
+            Err(_) => Ok(HttpResponse::InternalServerError().into()),
+        })
+        .responder()
 }
 
 fn main() {
-    server::new(|| {
-        App::new()
-            .resource("/", |r| r.f(greet))
-            .resource("/auth/token/get", |r| r.f(tokenGet))
-    })
-    .bind("127.0.0.1:8000")
-    .expect("Can not bind to port 8000")
-    .run();
+    ::std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    let sys = actix::System::new("diesel-example");
+
+    // Start 3 db executor actors
+    let manager = ConnectionManager::<SqliteConnection>::new("test.db");
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    let addr = SyncArbiter::start(3, move || DbExecutor(pool.clone()));
+
+    // Start http server
+    server::new(move || {
+        App::with_state(AppState{db: addr.clone()})
+            // enable logger
+            .middleware(middleware::Logger::default())
+            .resource("/{name}", |r| r.method(http::Method::GET).with(index))
+    }).bind("127.0.0.1:8080")
+        .unwrap()
+        .start();
+
+    println!("Started http server: 127.0.0.1:8080");
+    let _ = sys.run();
 }
