4b6d8b64a20aacc5241697a58e5db8f1c697a6ab
betula <betula@yandex-team.ru>
2018-06-21 21:43:56 +0300

    server: add feed router

--- server-node/src/app/App.js
+++ server-node/src/app/App.js
@@ -2,11 +2,13 @@ import { inject } from 'lib/core';
 import { Db } from 'services/Db';
 import { Server } from 'services/Server';
 import { AccountRouter } from 'routers/AccountRouter';
+import { FeedRouter } from 'routers/FeedRouter';
 
 export class App {
   @inject(Db) db;
   @inject(Server) server;
   @inject(AccountRouter) accountRouter;
+  @inject(FeedRouter) reedRouter;
 
   configure({ db, server }) {
     this.db.configure(db);
@@ -16,6 +18,7 @@ export class App {
   init() {
     this.db.init();
     this.accountRouter.init();
+    this.reedRouter.init();
   }
 
   run() {
---
+++ server-node/src/routers/FeedRouter.js
@@ -0,0 +1,15 @@
+import { inject } from 'lib/core';
+import { Route } from 'services/Route';
+
+export class FeedRouter {
+  @inject(Route) route;
+
+  init() {
+    this.route.make('GET', '/feed/list', this.list.bind(this));
+  }
+
+  async list() {
+    return { items: [] };
+  }
+
+}
