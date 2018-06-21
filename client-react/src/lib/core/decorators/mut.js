82697341f429283f08fcbae484d6cd2ba7429fd7
Sergey Sova <5620073+sergeysova@users.noreply.github.com>
2018-06-21 04:53:31 +0300

    Update mut.js

--- client-react/src/lib/core/decorators/mut.js
+++ client-react/src/lib/core/decorators/mut.js
@@ -33,10 +33,10 @@ export const mut = (Prototype, property) => {
       if (values[property] !== value) {
         values[property] = value;
 
-        if (!isReactComponent) {
-          this.__mutNotify();
-        } else {
+        if (isReactComponent) {
           this.forceUpdate();
+        } else {
+          this.__mutNotify();
         }
       }
     },
