fc946b3b38fe37431bd4cc15bae009fd9b2fc050
betula <mail@betula.co>
2019-07-10 00:34:08 +0800

    Pretty package scripts in api

--- api/start.ts
+++ api/start.ts
@@ -2,21 +2,27 @@ import "module-alias/register";
 
 import { App } from "~/app/App";
 import { provide } from "~/lib/core";
+import { Logger } from "~/services/Logger";
 
 class AppRunner {
   @provide public app: App;
+  @provide public logger: Logger;
 
   public async start() {
-    return this.app.start({
-      db: {
-        url: "mongodb://localhost:27017",
-        dbname: "sonata",
-      },
-      server: {
-        hostname: "127.0.0.1",
-        port: 2020,
-      },
-    });
+    try {
+      await this.app.start({
+        db: {
+          url: "mongodb://localhost:27017",
+          dbname: "sonata",
+        },
+        server: {
+          hostname: "127.0.0.1",
+          port: 2020,
+        },
+      });
+    } catch (error) {
+      this.logger.log(error);
+    }
   }
 }
 
