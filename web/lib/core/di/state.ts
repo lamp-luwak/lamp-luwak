4fefe98f126a93b1a33383bc6df136f4d39b705f
betula <mail@betula.co>
2019-12-15 21:12:04 +0300

    Some update for new project structure

--- web/lib/core/di/state.ts
+++ web/lib/core/di/state.ts
@@ -5,5 +5,4 @@ export default {
   instances: {} as ObjectMap<Map<Dep, any>>,
   overrides: {} as ObjectMap<Map<Dep, any>>,
   resolvePhases: {} as ObjectMap<Map<Dep, DepResolvePhase>>,
-  zoneDestroyListenerAdded: false,
 };
