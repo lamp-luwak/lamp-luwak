999f0cff0f06f3e724864b3f9e458f91adfb00f8
betula <mail@betula.co>
2020-01-21 22:03:15 +0800

    [react] Fix lint

--- packages/react/.eslintrc.js
+++ packages/react/.eslintrc.js
@@ -13,10 +13,10 @@ module.exports = {
   "plugins": ["@typescript-eslint"],
   "rules": {
     "quotes": ["error", "double"],
-    "no-prototype-builtins": 0,
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/no-use-before-define": 0,
     "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/no-non-null-assertion": 0
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "@typescript-eslint/no-unused-vars": 0
   },
 };
