da6104f4e39c5d851945578ed2d5c3bc076da55f
betula <mail@betula.co>
2020-01-25 22:11:39 +0800

    [hello-world] Cleanup boilerplate

--- examples/hello-world/src/App.test.tsx
+++ examples/hello-world/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("It Works", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const content = getByText(/World/i);
+  expect(content).toBeInTheDocument();
 });
