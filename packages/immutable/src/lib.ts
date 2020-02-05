fd5028b6c7c5568a39b4ae06e9580b75314b4bc3
betula <mail@betula.co>
2020-02-05 16:31:56 +0800

    [immutable] update api

--- packages/immutable/src/lib.ts
+++ packages/immutable/src/lib.ts
@@ -1,8 +1,13 @@
 import { Partial } from "./types";
 
-export function update<T>(struct: T, update: Partial<T>): T {
+export function update<T>(struct: T, upd: Partial<T>): T;
+export function update<T>(struct: T[], upd: T[]): T[];
+export function update(struct: any, upd: any): any {
+  if (Array.isArray(struct)) {
+    return [ ...struct, ...upd ];
+  }
   return {
     ...struct,
-    ...update
+    ...upd
   };
 }
--- packages/immutable/tests/lib.test.ts
+++ packages/immutable/tests/lib.test.ts
@@ -1,6 +1,6 @@
 import { update } from "~/lib";
 
-test("Should work update", () => {
+test("Should work update with objects", () => {
   const s1 = { a: 10, c: 5 };
   const s2 = { a: 11, b: 10 };
   const s3 = update(s1, s2);
@@ -8,4 +8,14 @@ test("Should work update", () => {
   expect(s3).not.toBe(s2);
   expect(s3).not.toBe(s2);
   expect(s3).toMatchObject({ a: 11, b: 10, c: 5});
+});
+
+test("Should work update with arrays", () => {
+  const s1 = [1, 2, 3];
+  const s2 = [5, 6];
+  const s3 = update(s1, s2);
+
+  expect(s3).not.toBe(s2);
+  expect(s3).not.toBe(s2);
+  expect(s3).toMatchObject([1, 2, 3, 5, 6]);
 });
