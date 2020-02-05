8c736f8f34dde9d894b4d70085fa45b35be26422
betula <mail@betula.co>
2020-02-05 11:45:10 +0800

    [react] Add prop-type container test

---
+++ tests/PropTypes.test.ts
@@ -0,0 +1,26 @@
+import { store, ContainerType } from "~/.";
+
+test("Should be work with containers", () => {
+  class A {
+    @store d = "D";
+  }
+  expect(ContainerType({ a: new A }, "a", "T")).toBeUndefined();
+  expect(ContainerType({ a: null }, "a", "T")).toBeUndefined();
+  expect(ContainerType({ a: {} }, "a", "T")).toMatchObject(
+    new Error("Invalid prop `a` supplied to `T`. Container with store props validation failed.")
+  );
+
+});
+
+test("Should be work isRequired", () => {
+  class A {
+    @store d = "D";
+  }
+  expect(ContainerType.isRequired({ a: new A }, "a", "T")).toBeUndefined();
+  expect(ContainerType.isRequired({ a: null }, "a", "T")).toMatchObject(
+    new Error("Invalid prop `a` supplied to `T`. Container with store props validation failed.")
+  );
+  expect(ContainerType.isRequired({ b: {} }, "b", "D")).toMatchObject(
+    new Error("Invalid prop `b` supplied to `D`. Container with store props validation failed.")
+  );
+});
