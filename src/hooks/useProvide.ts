322a45b1bcbb72e18b5221ad1e7c09cfb79fea73
betula <mail@betula.co>
2020-02-23 22:35:04 +0800

    [react] useProvide hook simplify

--- src/hooks/useProvide.ts
+++ src/hooks/useProvide.ts
@@ -1,19 +1,7 @@
-import { useState } from "~/driver";
 import { Dep } from "~/di/types";
 import { resolve } from "~/di";
-import { subscribe } from "~/store";
-import { useInvalidateEffect } from "./useInvalidateEffect";
-import { isShouldSubscribe } from "~/subscribe";
+import { useSubscribe } from "./useSubscribe";
 
 export function useProvide<T>(dep: Dep<T>): T {
-  const [[instance, shouldSubscribe]] = useState(() => {
-    const instance = resolve(dep);
-    return [ instance, isShouldSubscribe(instance) ];
-  });
-  if (shouldSubscribe) {
-    useInvalidateEffect(
-      (invalidate: any) => subscribe(instance, invalidate)
-    );
-  }
-  return instance;
+  return useSubscribe(() => resolve(dep));
 }
