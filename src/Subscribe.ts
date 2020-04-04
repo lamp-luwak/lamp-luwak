49e1bdafa3644b472701a27df54c228be202ecc6
betula <mail@betula.co>
2020-04-04 09:26:18 +0700

    [react] Fix Subscribe component typings

--- src/Subscribe.ts
+++ src/Subscribe.ts
@@ -1,9 +1,16 @@
+import { ReactNode, ReactElement } from "react";
 import { useSubscribe } from "./useSubscribe";
+import { ObjectMap } from "./types";
 
-export const Subscribe = (props: any) => {
+type Ret = ReactNode | ReactElement | null;
+type Props<T extends ObjectMap, R> = T & {
+  children: (props: T) => R;
+};
+
+export const Subscribe = <T, R = Ret>(props: Props<T, R>): R => {
   const values = Object.keys(props)
     .filter((key) => key !== "children")
-    .map((key) => props[key]);
+    .map((key) => (props as ObjectMap)[key]);
 
   useSubscribe(...values);
   return props.children(props);
