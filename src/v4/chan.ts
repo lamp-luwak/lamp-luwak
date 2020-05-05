d48c06e443671ed00cfdb19f8a63fce7e463f31f
betula <mail@betula.co>
2020-05-05 17:14:51 +0700

    [v4] Add diamond problem decision

--- src/v4/chan.ts
+++ src/v4/chan.ts
@@ -3,7 +3,10 @@ import { prop } from "./prop";
 const ChanReceivers = Symbol("ChanReceivers");
 const Multi = Symbol();
 
-let sendingDeepCounter = 0;
+const SendIterationLimit = 10000;
+let sendingDepth = 0;
+const onAfterLastDelegates = new Map();
+const onAfterLastDelegatesQueue = [] as any;
 
 function getChanReceivers(node: any) {
   return prop(node, ChanReceivers, []);
@@ -28,16 +31,48 @@ export function chan(from: any, to: any, callback?: (signal: any) => void) {
   });
 }
 
+function onAfterLast(callback: any) {
+  if (onAfterLastDelegates.has(callback)) {
+    return;
+  }
+  onAfterLastDelegates.set(callback, callback);
+  onAfterLastDelegatesQueue.push(callback);
+}
+
 export function receive(node: any, receiver: any) {
   if (node[Multi]) {
-    node.forEach((node: any) => receive(node, receiver));
+    let lastSignal = null as any;
+    let hasLastSignal = false;
+    const finish = () => {
+      const _hasLastSignal = hasLastSignal;
+      const _lastSignal = lastSignal;
+      lastSignal = null;
+      hasLastSignal = false;
+      if (_hasLastSignal) {
+        receiver(_lastSignal);
+      }
+    };
+    const multiReceiver = (signal: any) => {
+      onAfterLast(finish);
+
+      if (!hasLastSignal) {
+        hasLastSignal = true;
+        lastSignal = signal;
+      }
+      else if (lastSignal !== signal) {
+        const _lastSignal = lastSignal;
+        lastSignal = signal;
+        receiver(_lastSignal);
+      }
+    };
+    node.forEach((node: any) => receive(node, multiReceiver));
     return;
   }
   getChanReceivers(node).push(receiver);
 }
 
 export function send(node: any, signal: any) {
-  sendingDeepCounter ++;
+  sendingDepth ++;
   try {
     if (node[Multi]) {
       node.forEach((node: any) => send(node, signal));
@@ -47,8 +82,25 @@ export function send(node: any, signal: any) {
     for (const receiver of receivers) {
       receiver(signal);
     }
+
+    if (sendingDepth === 1) {
+      let iteration = 0;
+      while(onAfterLastDelegatesQueue.length > 0) {
+        const onAfterLast = onAfterLastDelegatesQueue.shift();
+        onAfterLastDelegates.delete(onAfterLast);
+        onAfterLast();
+        iteration ++;
+        if (iteration > SendIterationLimit) {
+          throw new Error("Send iteration limit exceeded");
+        }
+      }
+    }
   }
   finally {
-    sendingDeepCounter --;
+    if (sendingDepth === 1) {
+      onAfterLastDelegates.clear();
+      onAfterLastDelegatesQueue.length = 0;
+    }
+    sendingDepth --;
   }
 }
